aa87d6feb3ebbb80ce7fc4ff3b26187e24639538
jweber <john@jweber.org>
2011-06-28 15:54:29 -0500

    initial commit

---
+++ Munin.WinNode/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Munin.WinNode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Task(() => new TcpServer()).Start();
+
+            Console.WriteLine("Press enter to stop");
+            while(true)
+            {
+                var readLine = Console.ReadLine() ?? "";
+                switch (readLine.ToLowerInvariant())
+                {
+                    case "cls":
+                        Console.Clear();
+                        break;
+                    default:
+                        return;
+                }
+            }
+        }
+    }
+}
---
+++ Munin.WinNode/TcpServer.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+
+namespace Munin.WinNode
+{
+    class TcpServer
+    {
+        public TcpServer()
+        {           
+            var listener = new TcpListener(IPAddress.Any, 4949);
+            listener.Start(1);
+            listener.BeginAcceptTcpClient(ReceiveTcpClient, listener);
+
+            Console.WriteLine("Waiting for connection");
+        }
+
+        string NormalizeMessage(string message)
+        {
+            message = message.ToUpper();
+            message = Regex.Replace(message, @"\r?\n", string.Empty);
+            return message;
+        }
+
+        void ReceiveTcpClient(IAsyncResult asyncResult)
+        {
+            var listener = (TcpListener) asyncResult.AsyncState;
+            if (listener.Server.IsBound)
+            {
+                var client = listener.EndAcceptTcpClient(asyncResult);
+                Console.WriteLine("Received connection from {0}", client.Client.RemoteEndPoint);
+
+                var data = new byte[client.ReceiveBufferSize];
+                var dataString = new StringBuilder();
+                using (var stream = client.GetStream())
+                {
+                    var welcomeMessage = Encoding.ASCII.GetBytes("Connected to Munin.WinNode\n");
+                    stream.Write(welcomeMessage, 0, welcomeMessage.Length);
+
+                    int readCount;
+                    while ((readCount = stream.Read(data, 0, client.ReceiveBufferSize)) != 0)
+                    {
+                        dataString.Append(Encoding.ASCII.GetString(data, 0, readCount));
+                        if (Regex.IsMatch(dataString.ToString(), @"\r?\n"))
+                        {
+                            string message = NormalizeMessage(dataString.ToString());
+                            
+                            if (message == "QUIT")
+                                break;
+
+                            MessageHandler(stream, message);
+                            dataString.Clear();
+                        }
+                    }
+
+                    Console.WriteLine("Closing connection from {0}", client.Client.RemoteEndPoint);
+                }
+
+                client.Close();
+
+                listener.BeginAcceptTcpClient(ReceiveTcpClient, listener);
+            }
+        }
+
+        static void MessageHandler(NetworkStream stream, string message)
+        {
+            Console.WriteLine(message);
+            var echoMessage = Encoding.ASCII.GetBytes("Echo: " + message);
+            stream.Write(echoMessage, 0, echoMessage.Length);
+        }
+    }
+}
